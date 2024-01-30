23537778011adcda1f1f897b07961f032c453d00
lucas r. c. <lucas_rc15@live.com>
2024-01-30 01:22:47 -0300

    Added social media

--- data/teamlab.js
+++ data/teamlab.js
@@ -17,9 +17,9 @@ const dataLAB = [
       position: 'Especialista em Inteligência Tributária',
       src: '/img/lab/Maiara-Zacchi.jpg',
       social: [
-        { title: 'IG', url: 'https://www.instagram.com/nakamaestudio/' },
-        { title: 'G', url: 'https://maps.app.goo.gl/VNEazTPkqEnn7DbL9' },         
-        { title: 'WEB', url: 'https://www.estudionakama.com.br' }
+        { title: 'IG', url: 'https://www.instagram.com/strattax/' },
+        { title: 'IN', url: 'https://www.linkedin.com/in/maiara-zacchi-70b31a117?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' },         
+        
       ]
     },
     {
@@ -28,10 +28,10 @@ const dataLAB = [
         position: 'Consultora Financeira e Estratégica',
         src: '/img/lab/Camila-Dutra.jpg',
         social: [
-          { title: 'IG', url: '#' },   
-          { title: 'G', url: '#' },     
-        { title: 'IN', url: '#' },
-        { title: 'WEB', url: '#' }
+          { title: 'IG', url: 'https://www.instagram.com/camilacsd?igsh=dm1ucjN1bnNoa244' },   
+           
+        { title: 'IN', url: 'https://www.linkedin.com/in/consultoracamilasdutra' },
+        
         ]
       },
       {
@@ -40,10 +40,9 @@ const dataLAB = [
         position: 'Empreendedora e Franqueada',
         src: '/img/lab/Fernanda-Medeiros.jpg',
         social: [
-          { title: 'IG', url: '#' },
-          { title: 'G', url: '#' },
-          { title: 'IN', url: '#' },
-          { title: 'WEB', url: '#' },
+          { title: 'IG', url: 'https://www.instagram.com/mais1cafe.palhoca?igsh=NXI3YjlrbnZ0eGVi&utm_source=qr' },       
+         
+          
         ]
       }
   ];
