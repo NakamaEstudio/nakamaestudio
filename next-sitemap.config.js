2d8c0c341701f9932a5a1f8f4fe5bfa2d1e5ea02
lucas r. c. <lucas_rc15@live.com>
2024-02-29 20:42:10 -0300

    Sitemap and robots add

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,11 @@ module.exports = {
     siteUrl: 'https://www.estudionakama.com.br',
     generateRobotsTxt: true, // (opcional)
     // ...outras configurações
+    robotsTxtOptions: {
+    additionalSitemap: [
+      'https://www.estudionakama.com/server-sitemap-index.xml'
+    ],
+  },
+  
   }
 
